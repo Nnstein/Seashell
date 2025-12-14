263e8cffdbe6df9eda7b52c1d4e1654ac8b6fb72
Nnstein <akachinnstein@gmail.com>
2025-12-14 23:33:06 +0300

    feat: configure Vite for housekeeping and housekeeping management apps, setting base paths

--- Seashell-Housekeeping-Management-App/vite.config.ts
+++ Seashell-Housekeeping-Management-App/vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-<<<<<<< HEAD
     base: '/housekeeping-management/'
-=======
->>>>>>> 60623e5cf20839f6fd2cb711a4a8188c208d785a
 })
